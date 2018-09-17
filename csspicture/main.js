bbd176bacb4e22a215614b9d384a94f45b4f6f23
suriii <kopkop456789@gmail.com>
2018-09-17 16:58:00 +0800

    four commit

---
+++ csspicture/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+       var c=0;
+       setInterval(function(){
+           $("#wind").css("transform","rotate("+c+"deg)");
+           c=c+30;
+       },50);    
+});
+
+
+
+     
